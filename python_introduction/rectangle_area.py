cd23ab3c9a020b02570bb52135786b42885b1efc
= <=>
2025-11-05 19:29:13 +0000

    area of a rectangle

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,6 @@
+# A python script that evaluate the area of a rectangle
+length = 10
+width = 5
+# The area of a rectangle is defined as the product of its length and its widths
+area = length * width
+print("The area of the rectangle is: ", area)
