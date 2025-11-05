e1671fa5a09244e984834d98641418adf7e69358
= <=>
2025-11-05 19:47:44 +0000

    future age calculation

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,5 @@
+# A python script that calcultates the the future age of a person.
+# Promt the user to input their current age.
+current_age = int(input("How old are you?"))
+age_in_2050 = current_age + 27
+print("In 2050, you will be", age_in_2050, "years old.")
