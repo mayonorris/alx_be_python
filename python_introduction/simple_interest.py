9bbe9d3e2e14891dc200d3113ebcbbdeffb2ea4a
= <=>
2025-11-05 19:24:19 +0000

    simple intrest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,7 @@
+# A python script that compute the simple interest earned on an investment over a period of time
+
+principal = 1000
+rate = 0.05
+time = 3
+Interest  = principal * rate * time
+print("The simple interest is: ", Interest)
