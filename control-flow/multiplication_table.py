f250c8e914759fb9bf606309f904a66af6cd7f2d
= <=>
2025-11-13 00:10:21 +0000

    multiplication table

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,4 @@
+# Multiplication Table Generator :Use a for loop to generate and print the multiplication table for a given number.
+number = int(input("Enter a number to see its multiplication table: "))
+for i in range(1, 11):
+    print(f"{number} * {i} = ", number * i)
